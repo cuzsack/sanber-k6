d1ce67917e9005202e5163bae6e88f52a481ca68
Widy Dwicahyo <widy.dwicahyo@gmail.com>
2025-05-14 16:58:19 +0700

    Initial Push loadTest and loadScenario

--- scenarioTest.js
+++ scenarioTest.js
@@ -4,22 +4,30 @@ import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporte
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export let options = {
-vus: 80, // Virtual user
-duration: '30s', // Durasi pengujian
+  scenarios: {
+    constant_rate_test: {
+      executor: 'constant-arrival-rate',
+      rate: 25, // iterations per timeUnit
+      timeUnit: '500ms', // time unit
+      duration: '1m', // total duration of test
+      preAllocatedVUs: 10, // initial VUs to allocate
+      maxVUs: 1000, // max VUs to allow
+    },
+  },
 };
 
 export default function () {
-    const resp = http.get('http://test.k6.io/')
+    const resp = http.get('http://test.k6.io/');
     sleep(1);
     check(resp, {
         'status is 200': (r) => r.status === 200,
-        'response time is less than 400ms': (r) => r.timings.duration < 400,        
+        'response time is less than 400ms': (r) => r.timings.duration < 400,
     });
 }
 
 export function handleSummary(data) {
     return {
-        './report/reportLoad.html': htmlReport(data),
+        './report/reportScenario.html': htmlReport(data),
         stdout: textSummary(data, { indent: ' ', enableColors: true }),
     };
 }
